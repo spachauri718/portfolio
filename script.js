85d032d6cf020149170ac1edbca3b4a4d38f9c25
spachauri718 <spachauri718@gmail.com>
2023-12-09 14:10:27 +0530

    home sction done

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text" , {
+    strings: ["Mern Stack Developer", "Front End Developer", "Java Developer", "Web Designer"],
+    typeSpeed : 50,
+    backSpeed :50,
+    backDelay :1000,
+    loop :true
+})
